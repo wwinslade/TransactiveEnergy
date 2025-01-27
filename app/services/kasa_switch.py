be449d825e6e06ab6d9264196ec50573279276f0
adr-pi <pi@adr-pi.local>
2025-01-27 12:52:07 -0500

    Made ipv4 a protected member of KasaSwitch

--- app/services/kasa_switch.py
+++ app/services/kasa_switch.py
@@ -10,20 +10,21 @@ class KasaSwitch(AsyncDevice):
   def __init__(self, name, ipv4):
     """Init for KasaSwitches"""
     self.name = name
-    self.ipv4 = ipv4
+    self._ipv4 = ipv4
 
     # Call the Kasa library
   async def on(self):
-    self._switch = SmartPlug(self.ipv4)
+    self._switch = SmartPlug(self._ipv4)
     await self._switch.turn_on()
     
     self._status = True
+    print(f"DEBUG: Switch {self.name} @ {self._ipv4} successfully turned on")
 
   async def off(self):
-    self._switch = SmartPlug(self.ipv4)
+    self._switch = SmartPlug(self._ipv4)
     await self._switch.turn_off()
 
     self._status = False
-    print(f"DEBUG: Switch {self.name} @ {self.ipv4} successfully turned off")
+    print(f"DEBUG: Switch {self.name} @ {self._ipv4} successfully turned off")
 
   
