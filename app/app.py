f897c4c79969a79565af2682b85a04eb87a7ae4e
adr-pi <pi@adr-pi.local>
2025-01-28 12:29:27 -0500

    Documentation update

--- app/app.py
+++ app/app.py
@@ -1,7 +1,11 @@
+# app.py
+# Created by William Winslade on 28 Jan 2025
+
+# This file contains the main flask code for the web app
+
 from flask import Flask, render_template
 
 from services import *
-
 
 app = Flask(__name__)
 
@@ -9,8 +13,6 @@ app = Flask(__name__)
 @app.route("/")
 def root():
   return render_template("index.html")
-
-
 
 
 if __name__ == "__main__":
