798338e1c99f7ecdf86f90f83e290f19d3020518
adr-pi <pi@adr-pi.local>
2025-01-28 12:24:54 -0500

    Created ubibot.py to define Ubibot sensor object and API calls

---
+++ app/services/ubibot.py
@@ -0,0 +1,38 @@
+# ubibot.py
+# Created by William Winslade on 28 Jan 2025
+
+import requests
+import json
+import os
+
+class UbibotSensor:
+  def __init__(self, api_key, channel):
+    self.api_key = api_key
+    self.channel = channel
+  
+  def get_temp(self):
+    api_url = str("https://api.ubibot.com/channels/%s?account_key=%s"%(self.channel, self.api_key))
+    api_response = requests.get(api_url)
+    
+    if api_response.status_code == 200:
+      data = api_response.json()
+      with open("data.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+      tmp = data["channel"]["field1"]
+
+      if type(tmp) == float:
+        print(f"DEBUG: Ubibot API returned temp of {tmp}")
+      else:
+        print(f"WARN: Ubibot API returned a non-numerical temp value")      
+
+      return tmp
+    
+    else:
+      print(f"Ubibot API call failed with status code {api_response.status_code}")
+      return None
+
+
+    
+
+    
