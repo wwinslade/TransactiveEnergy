2cf9c588dd05e844d770913d9932f94b7cb19528
adr-pi <pi@adr-pi.local>
2025-02-03 13:26:19 -0500

    Created new Django apps 'dashboard' and 'users'

--- TransactiveEnergy/settings.py
+++ TransactiveEnergy/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.users',
+    'apps.dashboard',
     'apps.devices',
     'django.contrib.admin',
     'django.contrib.auth',
