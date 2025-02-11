2f3207f49917f10fbcf17a7feb52c771c4dc32ce
adr-pi <pi@adr-pi.local>
2025-02-11 12:09:01 -0500

    Added a basic operator panel for testing

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
+  path('operator/', views.admin, name='admin')
 ]
--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
+
+@login_required()
+def admin(request):
+
+  context = {}
+  return render(request, 'admin.html')
+
+def CreateNewDevice(request):
+
+  context = {}
+  return render(request, )
+
