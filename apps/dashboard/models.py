1517635ecfec0670864257d93d62c5ae03604705
adr-pi <pi@adr-pi.local>
2025-02-13 16:00:04 -0500

    Moved device DB models to devices subapp

--- apps/dashboard/models.py
+++ apps/dashboard/models.py
@@ -1,15 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Device(models.Model):
-  name = models.CharField(max_length=100, default='New Device')
-  description = models.TextField(null=True, blank=True)
-  device_type = models.CharField(max_length=50, default='Unknown')
-  status = models.BooleanField(default=False)
-  ipv4 = models.GenericIPAddressField(null=True, blank=True)
-  sync_or_async = models.CharField(max_length=10, default='sync')
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
-
-  def __str__(self):
-    return f'{self.device_type}.{self.name}'
--- apps/devices/models.py
+++ apps/devices/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+# Generic model for a device -- can be extended to include more specific device types
+class Device(models.Model):
+  DEVICE_TYPES = [
+    ('generic', 'Generic Device'),
+    ('kasa_switch', 'Kasa Switch'),
+    ('fridge', 'Fridge'),
+  ]
+
+  uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+  name = models.CharField(max_length=100, default='NewGenericDevice', null=True, blank=True)
+  type = models.CharField(max_length=20, choices=DEVICE_TYPES, default='generic')
+  description = models.TextField(null=True, blank=True)
+  status = models.BooleanField(default=False)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return f'{self.uuid}.{self.type}.{self.name}'
+
+# Model for KasaSwitches
+class KasaSwitch(models.Model):
+  device = models.OneToOneField(Device, on_delete=models.CASCADE, primary_key=True)
+  ip_address = models.GenericIPAddressField()
+
+  def __str__(self):
+    return f'{self.device.uuid}.{self.device.type}.{self.device.name}'
+  
+# Model for the fridge
+class Fridge(models.Model):
+  device = models.OneToOneField(Device, on_delete=models.CASCADE, primary_key=True)
+  high_temp_threshold = models.FloatField(default=50.0)
+  low_temp_threshold = models.FloatField(default=20.0)
+  target_temp_threshold = models.FloatField(default=38.0)
+
+  def __str__(self):
+    return f'{self.device.uuid}.{self.device.type}.{self.device.name}'
