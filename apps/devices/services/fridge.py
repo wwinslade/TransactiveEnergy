3b876ed6936396e969b97ab1ac4599126ba9b96e
adr-pi <pi@adr-pi.local>
2025-02-28 13:57:17 -0500

    Fixed unintended behavior of fridge relay board

--- apps/devices/services/fridge.py
+++ apps/devices/services/fridge.py
@@ -15,33 +15,26 @@ with using the Pi's GPIO pins. The RPi.GPIO library is used to that end.
 class FridgeAPI(SyncDevice):
   """class Fridge is a child class of the interface SyncDevice"""
   
-  def __init__(self, name):
-    super().__init__(name)
-    self.type = "GPIO Controlled Minifridge"
-    
+  def __init__(self):
     # Setup the GPIO API
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(4, GPIO.OUT, initial=GPIO.HIGH)
 
-  def __del__(self):
-    '''This destructor is called when the fridge object is to be deleted. Will cleanup the RPi GPIO library'''
+  # def __del__(self):
+  #   '''This destructor is called when the fridge object is to be deleted. Will cleanup the RPi GPIO library'''
     
-    GPIO.cleanup()
-    print(f"DEBUG: Fridge {self.name} is being deleted, cleaning up GPIO")
+  #   GPIO.cleanup()
+  #   print(f"DEBUG: Fridge is being deleted, cleaning up GPIO")
 
   def on(self):
     """Turn the fridge on"""
     GPIO.output(4, GPIO.LOW)
-
-    self._state = True
-    print(f"DEBUG: Fridge {self.name} was turned on successfully")
+    print(f"DEBUG: Fridge was turned on successfully")
 
   def off(self):
     """Turn the fridge off"""
     GPIO.output(4, GPIO.HIGH)
-
-    self._state = False
-    print(f"DEBUG: Fridge {self.name} was turned off successfully")
+    print(f"DEBUG: Fridge was turned off successfully")
 
   def enable_adr(self):
     raise NotImplementedError
