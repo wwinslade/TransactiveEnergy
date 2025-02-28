6377f3e0a0f2d0494c018800f2c57afeda45b88d
adr-pi <pi@adr-pi.local>
2025-02-28 13:57:41 -0500

    Added user and device subapps to main app URLs

--- TransactiveEnergy/urls.py
+++ TransactiveEnergy/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.dashboard.urls')),
-    path('user/', include('apps.users.urls'))
+    path('user/', include('apps.users.urls')),
+    path('device/', include('apps.devices.urls')),
 ]
