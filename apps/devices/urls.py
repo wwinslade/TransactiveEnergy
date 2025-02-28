7edc993661eab30d5775d036d63d859c987c17ee
adr-pi <pi@adr-pi.local>
2025-02-28 13:58:59 -0500

    Added fridge URLs and Iotawatt query URLs

--- apps/devices/urls.py
+++ apps/devices/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
   path('kasa_switch/<str:uuid>/on', views.KasaSwitchOn, name='kasa_switch_on'),
   path('kasa_switch/<str:uuid>/off', views.KasaSwitchOff, name='kasa_switch_off'),
+  path('fridge/<str:uuid>/on', views.FridgeOn, name='fridge_on'),
+  path('fridge/<str:uuid>/off', views.FridgeOff, name='fridge_off'),
+  path('iotawatt/query/', views.QueryIotaWatt, name='iotawatt_query'),
 ]
