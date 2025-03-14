aea4719fac298aa5e01f9e342a7b2fd198e92608
wwinslade <wwinslade01@gmail.com>
2025-03-14 14:59:15 -0400

    Created kasa switch on celery task

--- apps/devices/tasks.py
+++ apps/devices/tasks.py
@@ -1,48 +1,17 @@
 from apps.devices.models import Device, KasaSwitch, Fridge, EnergyConsumption
 
 from celery import shared_task
-import requests
 
-from datetime import datetime
-import logging
+from apps.devices.services.kasa import KasaSwitchAPI
 
-logger = logging.getLogger(__name__)
+import asyncio
 
-def query_iotawatt(query_range, sample_interval):
-  '''
-  Queries IotaWatt device for energy consumption data at a given time interval and sample interval.
-  query_range: Tuple of start and end timestamps for the query, must be in ISO relative time format
-  sample_interval: Time interval for sampling the data, must be in ISO format
+@shared_task
+def CeleryKasaSwitchOn(uuid):
+  switch = KasaSwitch.objects.get(device__uuid=uuid)
+  switch_api = KasaSwitchAPI(switch.ip_address)
 
-  For more time format information: https://docs.iotawatt.com/en/master/query.html#relative-time
-  '''
-  url = f'http://192.168.0.111/query?select=[time.iso,input_0,Meter1,Solar]&begin={query_range[0]}&end={query_range[1]}&group={sample_interval}&format=json&header=yes'
-  response = requests.get(url)
-  if response.status_code != 200:
-    logger.error(f'Failed to fetch data from IotaWatt: {response.status_code}')
-    return None
+  asyncio.run(switch_api.on())
 
-  times = response.json().get('range', [])
-  times = [datetime.fromtimestamp(time) for time in times]
-  
-  labels = response.json().get('labels', [])
-  data = response.json().get('data', [])
-
-  print(f'IotaWatt Data fetched for {sample_interval} interval from {times[0]} to {times[-1]}')
-
-  # Save the data to the EnergyConsumption model
-  for d in data:
-    for i, label in enumerate(labels[2:], start=2):
-      EnergyConsumption.objects.create(
-        timestamp=times[i],
-        device_name=label,
-        consumption=d[i]
-      )
-
-  logger.info(f'Energy consumption data saved to DB for {sample_interval} interval from {times[0]} to {times[-1]}')
-
-if __name__ == "__main__":
-  # Example usage
-  query_range = ('d-1d', 'd')
-  sample_interval = '10m'
-  query_iotawatt(query_range, sample_interval)
+  switch.device.status = True
+  switch.device.save()
