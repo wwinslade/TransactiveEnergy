cf7edd4b051a89d065c1ce41f80d0bbb356f2ef7
wwinslade <wwinslade01@gmail.com>
2025-04-01 12:29:10 -0400

    Created a scheduler for turning on/off deviced at specified time intervals

--- apps/devices/apps.py
+++ apps/devices/apps.py
@@ -5,3 +5,7 @@ class DevicesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.devices'
     verbose_name = 'Devices'
+
+    def ready(self):
+        from .scheduler import start
+        start()
--- apps/devices/scheduler.py
+++ apps/devices/scheduler.py
@@ -3,7 +3,11 @@ from apscheduler.executors.pool import ThreadPoolExecutor
 
 from datetime import datetime
 
-from .models import Device
+from .models import Device, KasaSwitch, Fridge
+from apps.devices.services.kasa import KasaSwitchAPI
+from apps.devices.services.fridge import FridgeAPI
+
+import asyncio
 
 executors = {
   'default': ThreadPoolExecutor(4),
@@ -11,18 +15,69 @@ executors = {
 
 scheduler = BackgroundScheduler(executors=executors)
 
-def trigger_device_event(device_uuid, action):
-  return
+def device_turn_on(uuid, type):
+  if type == 'kasa_switch':
+    switch = KasaSwitch.objects.get(device__uuid=uuid)
+    switch_api = KasaSwitchAPI(switch.ip_address)
+    asyncio.run(switch_api.off())
+
+    switch.device.status = True
+    switch.device.save()
+  elif type == 'fridge':
+    fridge = Fridge.objects.get(device__uuid=uuid)
+    fridge_api = FridgeAPI()
+
+    fridge_api.on()
+    
+    fridge.device.status = True
+    fridge.device.save()
+  else:
+    print('Device on task failed: Unknown device type')
+
+def device_turn_off(uuid, type):
+  if type == 'kasa_switch':
+    switch = KasaSwitch.objects.get(device__uuid=uuid)
+    switch_api = KasaSwitchAPI(switch.ip_address)
+    asyncio.run(switch_api.off())
+
+    switch.device.status = True
+    switch.device.save()
+  elif type == 'fridge':
+    fridge = Fridge.objects.get(device__uuid=uuid)
+    fridge_api = FridgeAPI()
+
+    fridge_api.on()
+    
+    fridge.device.status = True
+    fridge.device.save()
+  else:
+    print('Device off task failed: Unknown device type')
 
 def start():
   if not scheduler.running:
     scheduler.start()
 
-    device_schedules = []
-
     devices = Device.objects.filter(use_user_window=True)
     for d in devices:
-      device_schedules.append({'device_uuid': d.uuid, 'on_window_begin': d.on_window_begin, 'on_window_end': d.on_window_end})
+      data = {'device_uuid': d.uuid, 'device_type': d.type, 'on_window_begin': d.on_window_begin, 'on_window_end': d.on_window_end}
+      scheduler.add_job(
+        device_turn_on,
+        'cron',
+        hour = d.on_window_begin.hour,
+        minute = d.on_window_begin.minute,
+        args = [d.uuid, d.type],
+        id = f'device_{d.uuid}_{d.type}_ON'
+      )
 
+      scheduler.add_job(
+        device_turn_off,
+        'cron',
+        hour = d.on_window_end.hour,
+        minute = d.on_window_end.minute,
+        args = [d.uuid, d.type],
+        id = f'device_{d.uuid}_{d.type}_ON'
+      )
+
+    print('Scheduler started')
 
 
